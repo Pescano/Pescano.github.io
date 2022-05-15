b7cad7e3e7156103e7f0f7dd5db7388b8b19547a
Pescano <88772018+Pescano@users.noreply.github.com>
2022-05-15 18:29:55 +0200

    Add files via upload

--- formularios_html5/ej_3/porcentaje.php
+++ formularios_html5/ej_3/porcentaje.php
@@ -4,43 +4,54 @@ $impuesto = $_POST['impuestos'];
 $promocion = $_POST['promocion'];
 if ($promocion == "descuento") {
 	if ($impuesto == "porcentaje1") {
-		$final = $precio + ($precio * 0.09);
-		$impuesto = "4";
+		$final = $precio - ($precio * 0.01);
+		$impuesto = "4%";
+		$descuento = $precio * 0.05;
+		$promocion = "descuento del 5%";
 	}
 	elseif ($impuesto == "porcentaje2") {
-		$final = $precio + ($precio * 0.12);
-		$impuesto = "7";
+		$final = $precio + ($precio * 0.02);
+		$impuesto = "7%";
+		$descuento = $precio * 0.05;
+		$promocion = "descuento del 5%";
 	}
 	elseif ($impuesto == "porcentaje3") {
-		$final = $precio + ($precio * 0.21);
-		$impuesto = "16";
+		$final = $precio + ($precio * 0.11);
+		$impuesto = "16%";
+		$descuento = $precio * 0.05;
+		$promocion = "descuento del 5%";
 	}
 	else {
-		$final = $precio + ($precio * 0.30);
-		$impuesto = "25";
+		$final = $precio + ($precio * 0.20);
+		$impuesto = "25%";
+		$descuento = $precio * 0.05;
+		$promocion = "descuento del 5%";
 	}
 }
 else {
 	if ($impuesto == "porcentaje1") {
 		$final = $precio + ($precio * 0.04);
-		$impuesto = "4";
+		$impuesto = "4%";
 	}
 	elseif ($impuesto == "porcentaje2") {
 		$final = $precio + ($precio * 0.07);
-		$impuesto = "7";
+		$impuesto = "7%";
 	}
 	elseif ($impuesto == "porcentaje3") {
 		$final = $precio + ($precio * 0.16);
-		$impuesto = "16";
+		$impuesto = "16%";
 	}
 	else {
 		$final = $precio + ($precio * 0.25);
-		$impuesto = "25";
+		$impuesto = "25%";
 	}
 }
 echo "<br/> &nbsp; DATOS RECIBIDOS";
 echo "<br/> &nbsp; Precio producto: ", $precio, " euros";
-echo "<br/> &nbsp; Impuestos del producto: ", $impuesto, "%";
+echo "<br/> &nbsp; Impuestos del producto: ", $impuesto;
 echo "<br/> &nbsp; Con la siguiente prompción: ", $promocion;
+if ($promocion == "descuento del 5%") {
+	echo "<br/> &nbsp; Se ha ahorrado: ", $descuento, " euros";
+}
 echo "<br/> &nbsp; El precio final del producto es: ", $final, " euros";
 ?>
--- introducción_a_javaScript/misfunciones.js
+++ introducción_a_javaScript/misfunciones.js
@@ -6,3 +6,11 @@ function primo (num) {
 	}
 	return true;
 }
+
+function factorial (n) {
+	var total = 1; 
+	for (i=1; i<=n; i++) {
+		total = total * i; 
+	}
+	return total; 
+}
